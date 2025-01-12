4390894e9dea5a9c9ba99c61e2a896df50a4ee3e
mdd-tabuk <md00700tabuk@gmail.com>
2025-01-12 14:51:44 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDB8TQNcAV7rv_x3eNoHySIGIQbJj3abBo",
+  authDomain: "realtimechat-3b01e.firebaseapp.com",
+  projectId: "realtimechat-3b01e",
+  storageBucket: "realtimechat-3b01e.firebasestorage.app",
+  messagingSenderId: "992812403640",
+  appId: "1:992812403640:web:8294af715e9e0443a29d4d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
